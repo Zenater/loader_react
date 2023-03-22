7c1bae6a8fe7805b57c8901b3353e0ca25f3fd4f
Zenater <zent230096@mail.ru>
2023-03-22 16:37:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Loader} from "./components/Loader/Loader";
+
+function App() {
+    return (
+        <div>
+            <Loader/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './Loader.css';
+
+export const Loader = () => {
+  const [progress, setProgress] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleStop = () => {
+    clearInterval(intervalRef.current!);
+    intervalRef.current = null;
+    setIsRunning(false);
+  };
+  const handleStart = () => {
+    intervalRef.current = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          handleStop();
+          return 0;
+        } else {
+          return prevProgress + 1;
+        }
+      });
+    }, 50);
+    setIsRunning(true);
+  };
+  const handleOnClick = () => isRunning ? handleStop() : handleStart();
+
+  useEffect(() => {
+    handleStart();
+    return () => {
+      clearInterval(intervalRef.current!);
+    };
+  }, []);
+
+  return (
+    <div className='container'>
+      <h1>Loading...</h1>
+      <div className='progress-bar'>
+        <div className='progress' style={{ width: `${progress}%` }} />
+        <div className='indicator'>{`${progress}%`}</div>
+      </div>
+      <button className='start-stop-button' onClick={handleOnClick}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+    </div>
+  );
+};
+
+
